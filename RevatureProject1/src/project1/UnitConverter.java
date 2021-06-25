1be5f1b95dc149f50fe6ff79a2fa2ee354352a21
Kevin Basta <kbasta1@toromail.csudh.edu>
2021-06-25 15:25:30 -0700

    Although might look simple, best answer I could think of.

--- RevatureProject1/src/project1/UnitConverter.java
+++ RevatureProject1/src/project1/UnitConverter.java
@@ -17,33 +17,29 @@ public class UnitConverter {
 		System.out.println("3: US Gallons to Imperial Gallons");
 		System.out.println("4: Quit");
 
-		int select;
-		Scanner UserInput = new Scanner(System.in);
-		select = UserInput.nextInt();
-
 		boolean choice = true;
 
-		while (choice == true) {
+		int repeat = 0;
+
+		while (choice) {
+			int select;
+			Scanner UserInput = new Scanner(System.in);
+			select = UserInput.nextInt();
+
 			switch (select) {
 			case 1:
-				//CupstoTeaspoons(num);
 				double tsp = num * 48;
 				System.out.println("Conversion from Cups to Teaspoons = " + tsp + " tsp");
-				choice = false;
 				break;
 
 			case 2:
-				//MilestoKiloMeters(num);
 				double kilometers = num * 1.60934;
 				System.out.println("Conversion from Miles to Kilometers = " + kilometers + " km");
-				choice = false;
 				break;
 
 			case 3:
-				//USGallonstoImperialGallons(num);
 				double ig = num * 0.832674;
 				System.out.println("Conversion from US Gallons to Imperial Gallons = " + ig + " Imperial Gallons");
-				choice = false;
 				break;
 
 			case 4:
@@ -51,48 +47,8 @@ public class UnitConverter {
 				System.exit(0);
 				choice = false;
 				break;
-			}		
-			UserInput.close();
+			}
 		}
+
 	}
-
-	public static double CupstoTeaspoons(double qty) {
-		// 1 cup is 48 teaspoons
-		//double cup = 1;
-		//double tsp = 48;
-		double tsp = qty * 48;
-		//double CtoTSP = qty * tsp;
-
-		// 1 tsp is 0.020 cups
-		//double TSPtoC = (qty / tsp);
-
-		//return CtoTSP;
-		return qty;
-	}
-
-	public static double USGallonstoImperialGallons(double qty) {
-		// 1 USG = 0.832674 IG
-		//double usg = 1;
-		//double ig = 0.832674;
-		double Ig = qty * 0.832674;
-		//double USGtoIG = usg * ig;
-		// 1 IG = 1.20095 USG
-
-		//return USGtoIG;
-		return qty;
-	}
-
-	public static double MilestoKiloMeters(double qty) {
-		// 1 meter is 0.001 km
-		//double m = 1;
-		//double km = 1000;
-
-		//double MtoKM = (m / km);
-		// 1 km is 1000 meter
-		double km = qty / 1000;
-		//double KMtoM = km * m;
-		return qty;
-		//return MtoKM;
-	}
-
 }
